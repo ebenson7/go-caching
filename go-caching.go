2cf446dcec4e1522bcc04cf0d46d4021fdf51203
Eric Benson <ericbenson24@gmail.com>
2023-09-16 04:07:00 +0000

    Added aging features along with page request feature. Will add cache updating along with serving HTTP server for cached pages.

--- go-caching.go
+++ go-caching.go
@@ -1,25 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"time"
-	"fmt"
+	"net/http"
+	"net/url"
+	"io"
 )
 
-const DefaultExpiration time.Duration = -1 // This is bad practice normally, but for local development this will be fine until I add expiration features.
+const (
+	DefaultExpiration time.Duration = 0
+	NoExpireTime time.Duration = -1
+)
+
 
 type Items struct {
 	Value any
 	ExpirationTime int64 // This is set to int64 since the default type for time.Duration is an int64 in nanoseconds. This is also why DefaultExpiration is time.Duration, so we can compare them.
+	Expired bool
 }
 
-/*func (item Items) isExpired() bool {
+func (item Items) isExpired() bool {
 	if item.ExpirationTime == 0 {
 		return false
 	}
 
-	return time.Now().UnixNano() > item.ExpirationTime
-}*/
+	if item.ExpirationTime <= time.Now().UnixNano() && item.ExpirationTime > 0 {
+		return true
+	}
+
+	return false
+}
 
 type Cache struct {
 	items map[string]Items
@@ -28,8 +40,7 @@ type Cache struct {
 }
 
 func (c *Cache) SetItem(k string, v any, ex time.Duration) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.Lock() // Ensure no one causes a race condition
 
 	var newExpireTime int64
 	
@@ -45,29 +56,131 @@ func (c *Cache) SetItem(k string, v any, ex time.Duration) {
 		Value: v,
 		ExpirationTime: newExpireTime, 
 	}
+
+	c.mu.Unlock()
 }
 
 func (c *Cache) GetItem(k string) {
 	c.mu.Lock()
-	defer c.mu.Unlock()
 	
 	fmt.Println(c.items[k])
+	c.mu.Unlock()
+}
+
+func (c *Cache) DeleteItem(k string) bool{
+	c.mu.Lock()
+	_, exists := c.items[k]
+	if !exists {
+		fmt.Println("Error, item does not exist and cannot be deleted.")
+	}
+
+	delete(c.items, k)
+	c.mu.Unlock()
+	return true
+}
+
+/* Below is non-exported functions used to create the same functionality while there are mutex locks to avoid deadlock. */
+
+/*func (c *Cache) setItem(k string, v any, ex time.Duration) bool {
+	var newExpireTime int64
+	
+	if ex == DefaultExpiration {
+		ex = c.Expiration
+	}
+
+	if ex > -1 {
+		newExpireTime = time.Now().Add(ex).UnixNano()
+	}
+
+	c.items[k] = Items{
+		Value: v,
+		ExpirationTime: newExpireTime, 
+	}
+
+	return true
+}
+
+func (c *Cache) retrieveItem(k string) {
+	fmt.Println(c.items[k])
+}*/
+
+func (c *Cache) deleteItem(k string) bool {
+	_, exists := c.items[k]
+	if !exists {
+		fmt.Println("Error, item does not exist and cannot be deleted.")
+	}
+
+	delete(c.items, k)
+	return true
+}
+
+func (c *Cache) maintainCache(ch chan<- bool) {
+	c.mu.Lock()
+	for k, v := range c.items {
+		if v.isExpired() {
+			c.deleteItem(k)
+		}
+	}
+	c.mu.Unlock()
+	ch <- true
+}
+
+func (c *Cache) RetrievePage(page string) {
+	url, err := url.ParseRequestURI(page)
+	if err != nil {
+		fmt.Println("URL is not valid.")
+	}
+	nurl := url.String()
+	resp, err := http.Get(url.String())
+	if err != nil {
+		fmt.Println("error getting page")
+	}
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.SetItem(nurl, body, DefaultExpiration)
+
 }
 
 func main() {
 	it := make(map[string]Items)
-	
+
 	cache := &Cache{
 		items: it,
 		Expiration: DefaultExpiration,
 	}
 
-	cache.SetItem("test", 12341234, -1)
-	cache.SetItem("test2", 234523634574, -1)
-	cache.SetItem("test5", "asfgdfshsdfhshsh", -1)
+	cache.RetrievePage("https://google.com")
+
+	ticker := time.NewTicker(time.Second * 10)
+	defer ticker.Stop()
+	done := make(chan bool)
+
+	fmt.Println(NoExpireTime)
+
+	cache.SetItem("test", 12341234, time.Second * 20)
+	cache.SetItem("test2", 234523634574, NoExpireTime)
+	cache.SetItem("test5", "asfgdfshsdfhshsh", time.Second * 10)
 	cache.GetItem("test")
 	cache.GetItem("test2")
 	cache.GetItem("test5")
+
+	for {
+		select {
+		case <-done:
+			fmt.Println("Cleaned cache.")
+		case <-ticker.C:
+			go cache.maintainCache(done)
+			fmt.Println("Tick")
+			cache.GetItem("https://google.com")
+			cache.GetItem("test")
+			cache.GetItem("test2")
+			cache.GetItem("test5")
+		}
+	}
 }
 
 
